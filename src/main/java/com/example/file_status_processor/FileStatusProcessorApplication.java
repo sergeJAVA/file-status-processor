3fddcf5101ca962a3c2d8ad3050d05342fd71163
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-08-24 11:15:35 +0300

    first commit

---
+++ src/main/java/com/example/file_status_processor/FileStatusProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.file_status_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStatusProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStatusProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/file_status_processor/FileStatusProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.file_status_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStatusProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
